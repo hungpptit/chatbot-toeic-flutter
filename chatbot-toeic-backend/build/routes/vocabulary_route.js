b9db74c1c41f44e6f48312fe9ea7ee50ebda9f9e
tanquy04 <nguyentanquy04@gmail.com>
2026-05-13 10:35:08 +0700

    first

--- chatbot-toeic-backend/build/routes/vocabulary_route.js
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-import { getVocabularyList, getVocabularyDetail, findOrFetchVocabulary } from '../controllers/vocabulary_controller.js';
-const router = express.Router();
-router.get('/', getVocabularyList);
-router.get('/:id', getVocabularyDetail);
-router.get('/word/:word', findOrFetchVocabulary);
-export default router;
